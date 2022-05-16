bb6b30cf94cd4ff007b4bae89d1e2a1d578df4b6
Shluha <Shluha@DESKTOP-MPSBJ0U>
2022-05-16 16:52:35 +0300

    Добавьте файлы проекта.

---
+++ StarodubtsevPR16(2)/StarodubtsevPR16(2)/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using StarodubtsevPR16;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace StarodubtsevPR16_2_
+{
+    public partial class MainPage : ContentPage
+    {
+        ApplicationViewModel viewModel;
+        public MainPage()
+        {
+            InitializeComponent();
+            viewModel = new ApplicationViewModel() { Navigation = this.Navigation };
+            BindingContext = viewModel;
+        }
+
+        protected override async void OnAppearing()
+        {
+            await viewModel.GetFriends();
+            base.OnAppearing();
+        }
+
+    }
+}
